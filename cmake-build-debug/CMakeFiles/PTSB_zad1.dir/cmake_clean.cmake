3a352dfea8a2f4f2f80a2f76f096430cfd435d5e
Shrulk <58909805+Shrulk@users.noreply.github.com>
2021-12-08 23:53:12 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/PTSB_zad1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PTSB_zad1.dir/main.cpp.obj"
+  "PTSB_zad1.exe"
+  "PTSB_zad1.exe.manifest"
+  "PTSB_zad1.pdb"
+  "libPTSB_zad1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PTSB_zad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include <array>
+
+using namespace std;
+
+double F(double x) {
+    return x;
+}
+
+int main() {
+    double p; // Каппа в нашей задаче, > 0
+    double q; // > 0
+    double x = 0, y = 0; // Координаты
+    array<double, 2> u = {x, y};
+
+    return 0;
+};
