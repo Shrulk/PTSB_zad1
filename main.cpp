da28db28b2daebd78669ab3247ed5c4e4390e83f
Shrulk <58909805+Shrulk@users.noreply.github.com>
2021-12-13 01:00:29 +0300

    Added Eigen to project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 // Решаем уравнение, описывающее стационарные проецссы, вида:
-//                                    -div(p(x) grad(u)) + q(x) u = F(x)
-#include "iostream"
+//       Lu = F          <=>            -div(p(x) grad(u)) + q(x) u = F(x)
+
+#include <iostream>
 #include <array>
+#include <vector>
 #include <functional>
-#include "cmath"
+#include <cmath>
+#include <D:/Informatics/CLionProjects/PTSB_zad1/Eigen/Eigen/Eigen.>
 
-using namespace std;
-
+using namespace Eigen;
+/*
 double F(double x) { // Неоднородность
     return x;
 }
@@ -39,3 +42,32 @@ int main() {
 
     return 0;
 };
+*/
+
+// Класс одного конечного элемента
+class Elem {
+    int num; // Номер элемента
+    double u_k;  // Значение U_k на этом элементе
+    double phi = 1; // Базисный вектор (всегда равен 1 на этом элементе: Phi_k(X_m) = Kroneker_km)
+
+};
+
+
+int main() {
+    // Область определения Омега
+    double x_start = 0, y_start = 0;
+    double x_end = 1, y_end = 1;
+
+    // Шаг сетки h (одинаков вдоль осей Ox и Oy)
+    double h = 0.01;
+
+    // Количество узлов n
+    double n = ((x_end - x_start) / h) * ((y_end - y_start) / h);
+    // Нумеровать узлы будем как: k = i + [(j-1) * (1/h)]
+
+    //vector<>
+    Matrix<float, 3, 3> m;
+
+
+    return 0;
+}
